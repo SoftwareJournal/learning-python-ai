c3c1eeda3710791544d30912f3f9b774da6d146a
unknown <mekkah.apps@gmail.com>
2024-05-23 23:17:54 -0500

    push number changes

--- python-files/numbers_and_math.py
+++ python-files/numbers_and_math.py
@@ -10,8 +10,10 @@
 # There are two number types in python: Integer and Float
 print (1/2) #division will always return a float
 print ((1+2)*5/3) #division will always return a float
-print (1+2*5/3) #division will always return a float
+print (1+2*5/3) #division will always return a float except for Integer Division with // operator
 print (2**3)
 print (10%3)
 print (10//3)
+print (6.5*2)
+print (6/2)
 # run file in Windows Shell as the following(change to file directory):  python .\numbers_and_math.py
