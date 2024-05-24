775c8a8bec2d4ebc60d89c4667419a92a630ac56
unknown <mekkah.apps@gmail.com>
2024-05-23 23:12:03 -0500

    Added number and math lessons

---
+++ python-files/numbers_and_math.py
@@ -0,0 +1,17 @@
+#       Symbol         |        Name
+#---------------------------------------
+#          +           |       Addition
+#          -           |       Substraction 
+#          *           |       Multiplication
+#          /           |       Division 
+#          **          |       Exponentiation
+#          %           |       Modulo (reminder operator)
+#          //          |       Integer Division
+# There are two number types in python: Integer and Float
+print (1/2) #division will always return a float
+print ((1+2)*5/3) #division will always return a float
+print (1+2*5/3) #division will always return a float
+print (2**3)
+print (10%3)
+print (10//3)
+# run file in Windows Shell as the following(change to file directory):  python .\numbers_and_math.py
