a09024b46070c8b9a55e8d639969a2ecb5334377
unknown <mekkah.apps@gmail.com>
2024-05-30 21:51:58 -0500

    variables and data types notes

---
+++ python-files/variables_and_datatypes.py
@@ -0,0 +1,21 @@
+# Understand how to assign and use variables 
+# Lean Python variable naming restrictions and conventions
+# Learn and use some of the different data types available in Python
+# Learn why Python is called a dynamically-typed language
+# Understand how to convert data types
+# Learn the ins and outs of Strings!
+
+num_of_cats = 99
+print(num_of_cats)
+print(num_of_cats*2)
+
+#Naming Restrictions
+#1 Variables must start with a letter or underscore
+_cats # good
+"2cats" # not good
+
+#2 The rest of the name must consist of letters, numbers, or underscores
+cats2 # good
+"hey@you" # not good
+
+
